370f924bf6a28c21f98bc38a3c6f7d9aee13888c
Serhiy Khalak <serghalak@gmail.com>
2020-05-04 13:39:04 +0300

    add 2 modules

---
+++ HW02-gradle/src/main/java/ru/otus/App.java
@@ -0,0 +1,9 @@
+package ru.otus;
+
+public class App {
+    public static void main(String... args) {
+        //HikariConfig config = new HikariConfig();
+        //System.out.println(config);
+        System.out.println("HW02 - Hello world!!!");
+    }
+}
